2ea749c00ec71bdcc6bea77b38873697c7128970
Veniamin Polienko <kasaklalita@mail.ru>
2022-02-04 19:06:48 +0300

    added All Posts & Single Post pages, fixed some typos

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Post
-
-def get_date(post):
-    return post['date']
 
 # Create your views here.
 def starting_page(request):
@@ -15,12 +12,14 @@ def starting_page(request):
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug = slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post
     })
