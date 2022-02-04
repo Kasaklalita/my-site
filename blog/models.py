1eadeeedadad4f97606f2d1f27bfdc8ac8958c12
Veniamin Polienko <kasaklalita@mail.ru>
2022-02-04 18:07:02 +0300

    created migrations & fixed typos

--- blog/models.py
+++ blog/models.py
@@ -14,9 +14,9 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length = 150)
     excerpt = models.CharField(max_length = 200)
-    image_name = modeld.CharField(max_length = 100)
+    image_name = models.CharField(max_length = 100)
     date = models.DateField(auto_now = True)
     slug = models.SlugField(unique = True, db_index = True)
     content = models.TextField(validators = [MinValueValidator(10)])
-    author = models.ForeignKey(Author, on_delete = models.SETNULL, related_name = "posts")
+    author = models.ForeignKey(Author, on_delete = models.SET_NULL, null = True, related_name = "posts")
     tags = models.ManyToManyField(Tag)
