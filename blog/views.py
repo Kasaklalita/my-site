6b4ef44481f6ab221a3335126d2784e18644ed9d
Veniamin Polienko <kasaklalita@mail.ru>
2022-02-06 18:52:09 +0300

    function-based views converted to class-based views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from datetime import date
 from .models import Post
-
+from django.views.generic import ListView, DetailView
 # Create your views here.
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
+
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3]
     # sorted_posts = sorted(all_posts, key = get_date)
